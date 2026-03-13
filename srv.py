98867c03c2fcdb82a28d1da194a9c0882691d899
VladVes <vccs@inbox.ru>
2026-03-13 21:29:29 +0700

    add simple example

---
+++ srv.py
@@ -0,0 +1,10 @@
+from wsgiref.simple_server import make_server
+from main import app
+
+def server(wsgi_app):
+    served = make_server("", 8000, wsgi_app)
+    print("Serving HTTP on port 8000...")
+    served.serve_forever()
+    
+if __name__ == "__main__":
+    server(app)
