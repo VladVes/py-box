35358d2156304b7b757491b0a641c5d6490ea085
VladVes <vccs@inbox.ru>
2026-03-20 14:13:04 +0700

    upd basics

--- types.py
+++ types.py
@@ -1,4 +1,12 @@
-# Как работают аннотации типов?
+# 22. Можно ли строки привести к bool
+s = 'some string'
+b = bool(s)
+print(b)
+
+# 23. какие примитивныйе типы есть в Python:
+print('TYPES: int, str, bool, float')
+
+# 24. Как работают аннотации типов?
 # Определить функцию test принимающую строку и целочисленное число и возварщающую bool.
 # Как указать что фукнция ничего не возвращает?
 
@@ -8,3 +16,5 @@ def test1(s: str, i: int) -> bool:
 
 def test2(flag: bool) -> None:
     print(f'Is it work\'s witho no return? {flag}')
+test1('snake', 5)
+test2(True)
