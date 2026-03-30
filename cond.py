ac4072c1d88412eb215c34fdf4f8e31c63b908d7
VladVes <vccs@inbox.ru>
2026-03-30 10:29:25 +0700

    upd basics with ternary operator

--- cond.py
+++ cond.py
@@ -6,3 +6,10 @@ elif x == 10:
     print('elif works')
 else:
     print('else')
+
+# 38. Как устроен тернарный оператор в Python? 
+# Написать пример функции get_type_of_sentence которая возвращает тип предложения
+# (question или normal) в зависимости от знака на которое оно заканчивается 
+
+def get_type_of_sentence(sent: str) -> str:
+    return 'question' if sent.strip().endswith('?') else 'normal'
