3d1e2e5fa81d65e28bb7176b76965ad428d112e6
VladVes <vccs@inbox.ru>
2026-03-31 10:14:46 +0700

    add match basic example

--- cond.py
+++ cond.py
@@ -13,3 +13,25 @@ else:
 
 def get_type_of_sentence(sent: str) -> str:
     return 'question' if sent.strip().endswith('?') else 'normal'
+
+# 39. Чем по сути является match в Python?
+# Набрать пример конструкции в функции get_status
+# которая получает значение в виде кода 100 - new, 200 - processing, 300 - paid
+# печатает и возвращает их текстовые значенияю.
+# Если код не совпадает то пишет Error
+
+def get_status(code: int) -> str:
+    match code:
+        case 100:
+            return 'NEW'
+        case 200:
+            return 'PROCESSING'
+        case 300:
+            return 'PAID'
+        case _:
+            return 'ERROR'
+
+print(get_status(100))
+print(get_status(200))
+print(get_status(300))
+print(get_status(0))
