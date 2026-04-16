714e5b72e033b4134d7823b1b9f5fc25e362664e
VladVes <vccs@inbox.ru>
2026-04-16 11:59:28 +0700

    update basics with typles example

---
+++ tuples.py
@@ -0,0 +1,11 @@
+rgb_color = (255, 127, 64)
+name_and_age = ("John", 88)
+three_bool = (True, False, True)
+pairs = ((1, 2), (3, 4))
+one_elemet_tuple = (1,)
+additional_syntax = 1, 2, 3 # no brackets
+print(rgb_color)
+print(len(rgb_color)) # has len
+
+
+
